d25a1544d1f6e788e3606a907231f9a10217f5bb
Drapeau L <laurent.drapeau@ird.fr>
2010-10-10 00:00:00 +0000

    version 1.1

--- R/onLoad.r
+++ R/onLoad.r
@@ -1,10 +1,9 @@
 .onLoad <- function(libname, pkgname) {
 	library(RGtk2)
-	
-	if (!is.null(try(gtkCheckVersion(2,16,0),T))) {
-		stop("This package needs gtk+ with version 2.16 or higher. ")
+		
+	if (!is.null(try(gtkCheckVersion(2,16,0),TRUE))) {
+		warning("This package needs gtk+ with version 2.16 or higher. ")
 	}
-
 	gtkListStoreGetType()
 	gtkWindowGetType()
 	gtkNotebookGetType()
